77756be7cefc10b40f1b21fa8766a1b974e701a5
Ryuu <Ryuu@10.140.117.197>
2010-09-21 15:29:40 -0700

    Here is the start

---
+++ src/edu/asu/driver.java
@@ -0,0 +1,18 @@
+package edu.asu;
+/**This program is to get the team familiar with the colloborative service of github.
+ * The program will print out 'Hello, team!' followed by everyone's first and last names. 
+ * @author Ryuu
+ *
+ */
+public class driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, team!");
+		System.out.println("My name is Steven Honda");
+	}
+
+}
